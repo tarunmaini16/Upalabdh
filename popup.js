6b7553c2e8d283d677049ba9f04d42a712669fc6
Tarun Maini <mainitarun7@gmail.com>
2020-05-10 19:57:48 +0530

    Uptated status.js & adding status.js

--- popup.js
+++ popup.js
@@ -1,27 +1,131 @@
-function func1(plusNewItem=' c') {
-	document.getElementById("amazon_list").textContent =  plusNewItem;
+var products = [];
+var esites = {amazon: 3, bigbasket: 5, flipkart: 3} //this part is index of url from where to fetch product name
+var getCurrentUrl;
+var product_name;
+var allshops;
+
+function setJSON(key, value) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getJSON(key) {
+    return JSON.parse(window.localStorage.getItem(key));
+}
+
+function product_del(id) {
+    let del_elem = document.getElementById("prod_list_" + id);
+    del_elem.classList.add("ptodel");
+
+    let product_id = 0;
+    // search product
+    for (let i = 0, i_cnt = products.length; i < i_cnt; i++) {
+        if (products[i].id == id) {
+            product_id = i;
+            break;
+        }
+    }
+
+    setTimeout(function () {
+        if (window.confirm('Do you want delete this product?')) {
+            del_elem.remove();
+            console.log(products);
+            products.splice(product_id, 1);
+            console.log(products);
+            setJSON('productList', products);
+        } else del_elem.classList.remove("ptodel");
+    }, 80);
 
 }
-// window.onload=func1;
-var listOfUrl = [];
-function getCurrentUrl(){
-	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-		var gctu = tabs[0].url;
-		var productNameToArray = gctu.split('/')
-		var esites = {a: 3, b: 5, f: 3, g: 4}
-		for (domain in esites){
-			if(productNameToArray[2].includes(domain)) {
-				document.getElementById("AddedUrl").innerHTML = '<br>Added to ' + domain  + '<h3 style="color:#1c22ff;background-color:#faff11">' + productNameToArray[esites[domain]];
-				// func1(productNameToArray[esites[domain]])
-			}
-		}
-		console.log('gctu-->>' + gctu);
-		console.log('urlSize-->>' + listOfUrl.push(gctu));
-		console.log('listOfUrl-->>' + listOfUrl);
-		localStorage["listOfUrl"] = listOfUrl;
-		document.getElementById("Amazon1").innerHTML = 'Added to ';
-	});
-}
-document.addEventListener('DOMContentLoaded', function () {
-	document.getElementById("getCurrentUrl").addEventListener("click", getCurrentUrl);
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.text == "cs") {
+        console.log("Popup says it was opened.");
+        products.forEach(prod =>
+            product_list_insert(prod, false)
+        );
+    }
 });
+
+function product_list_insert(prod, feature) {
+    let class_ins = '';
+    if (feature) {
+        // remove all features products
+        document.querySelectorAll('.pitem').forEach(function (e) {
+            e.classList.remove('pfeature');
+        });
+        class_ins = 'pfeature';
+    }
+
+    let prod_name = prod.name
+    // let target_elem = document.getElementById(shop_label[prod.shop] + "_list");
+    let target_elem = document.getElementById(prod.shop + "_list");
+
+    try {
+        target_elem.insertAdjacentHTML("beforeend", '<div id="prod_list_' +
+            prod.id + '" class="pitem ' + class_ins + '"><img src="' + prod.status + '.png"><div class="pname">' + prod_name +
+            ' </div><div id="dprod_' + prod.id + '" class="pdel"><img src="bin.png"></div></div>');
+    } catch (e) {
+        alert("Seems like we are not on the correct site")
+    }
+
+
+    var button = document.getElementById('dprod_' + prod.id);
+    button.onclick = function () {
+        product_del(prod.id)
+    };
+
+}
+
+function get_max_id() {
+    let max_id = 0;
+    for (let i = 0, i_cnt = products.length; i < i_cnt; i++) {
+        if (products[i].id > max_id) max_id = products[i].id;
+    }
+
+    return max_id;
+}
+
+function product_add() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        getCurrentUrl = tabs[0].url;
+        var productNameToArray = getCurrentUrl.split('/')
+
+        for (let i = 0, i_cnt = products.length; i < i_cnt; i++) {
+            if (products[i].url == getCurrentUrl) {
+                alert('This product is already added');
+                return;
+            }
+        }
+        for (domain in esites) {
+            if (productNameToArray[2].includes(domain)) {
+                var shop_id = domain
+                // console.log(esites[shop_id])  //work
+                // console.log(esites[domain])  //work
+                var getIndex = esites[shop_id]
+                product_name = productNameToArray[getIndex]
+            }
+        }
+        let new_prod = {shop: shop_id, name: product_name, id: (get_max_id() + 1), status: 0, url: getCurrentUrl}
+
+        products.push(new_prod)
+        // chrome.runtime.sendMessage({text: "item added"});
+        product_list_insert(new_prod, true);
+        setJSON('productList', products);
+    });
+}
+
+function diag() {
+    if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function product_list_init() {
+    document.getElementById('add_product').addEventListener("click", product_add);
+
+    products = getJSON('productList') || [];
+    products.forEach(prod =>
+        product_list_insert(prod, false)
+    );
+});
+// window.onload = product_list_init();
---
+++ status.js
@@ -0,0 +1,91 @@
+var set_interval = 30;
+var tryThis;
+var sourceOfTruth = 'prod';
+var notMes = ['UNKNOWN', 'AVAILABLE', 'UNDELIVERABLE', 'UNAVAILABLE']
+
+function getJSON(key) {
+    return JSON.parse(window.localStorage.getItem(key));
+}
+
+function setJSON(key, value) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+}
+
+var products = getJSON('productList') || [];
+
+function diag() {
+    if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+    }
+}
+
+function getLocators() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            tryThis = JSON.parse(this.response)
+            // console.log(tryThis)
+        }
+    };
+    if (sourceOfTruth == 'test') {
+        source = 'scratch.json'
+    } else
+        source = 'https://80006633-368a-4e97-bab2-9c94dee6afdd.mock.pstmn.io'
+    // xmlhttp.open("GET", "https://raw.githubusercontent.com/tarunmaini16/Upalabdh/master/locators.json", true);
+    // xmlhttp.open("GET", "https://bf8e93a4-c698-4fbc-a3b4-a3e0d530e69e.mock.pstmn.io", true);
+    xmlhttp.open("GET", source, true);
+    // xmlhttp.open("GET", "scratch.json", true);
+    xmlhttp.setRequestHeader('Cache-Control', 'no-cache');
+    xmlhttp.send();
+}
+
+function check_stat() {
+    console.log('in update')
+    getLocators();
+    products = getJSON('productList');
+    var xhr = [], i;
+    for (let i = 0, i_cnt = products.length; i < i_cnt; i++) { //for loop
+        (function (i) {
+            xhr[i] = new XMLHttpRequest();
+            xhr[i].responseType = 'document';
+            itemUrl = products[i].url;
+            xhr[i].open("GET", itemUrl, true);
+            xhr[i].onreadystatechange = function () {
+                if (xhr[i].readyState === 4 && xhr[i].status === 200) {
+                    for (let j = 0, j_cnt = products[i].shop; j < tryThis[j_cnt].length; j++) {
+                        // console.log( tryThis[j_cnt][j])
+                        // console.log("keys: "+ Object.keys(tryThis[j_cnt][j]) + " :: values: "+Object.values(tryThis[j_cnt][j]))
+                        var actual = xhr[i].response.querySelector(Object.keys(tryThis[j_cnt][j]))
+                        if (actual != null) {
+                            if (JSON.stringify(Object.values(tryThis[j_cnt][j])).includes(actual.innerText.trim())
+                                && (products[i].status != parseInt((JSON.stringify(Object.values(tryThis[j_cnt][j]))).charAt(2)))) {
+                                console.log('Before: i: ' + i + '  name: ' + products[i].name + '  id: ' + products[i].id + '  status: ' + products[i].status)
+                                let prev_status = products[i].status
+                                products[i].status = parseInt((JSON.stringify(Object.values(tryThis[j_cnt][j]))).charAt(2))
+                                var opt = {
+                                    type: "basic",
+                                    title: notMes[products[i].status] + " B-" + prev_status + ":  N-" + products[i].status,
+                                    message: products[i].name,
+                                    requireInteraction: true,
+                                    iconUrl: products[i].status + ".png"
+                                }
+                                chrome.notifications.create('Status' + products[i].id, opt, diag)
+                                setJSON('productList', products);
+                                console.log('After: i: ' + i + '  name: ' + products[i].name + '  id: ' + products[i].id + '  status: ' + products[i].status)
+                                break;
+                            } else if (Object.values(tryThis[j_cnt][j]) == null) {
+                                console.log("Facing issue, due to some change happened from backend, Raise ticket at @.com to get this fixed. Share some details with this url " + products[i].url)
+                            }
+                        }
+                    }
+                }
+            };
+            xhr[i].send();
+        })(i);
+    }
+    setTimeout(function () {
+        if (getJSON('productList').length > 0) check_stat()
+        else console.log('no product in list')
+    }, set_interval * 1000);
+};
+setTimeout(check_stat, 5 * 1000);
